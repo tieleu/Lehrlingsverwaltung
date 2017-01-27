fb9aa441965afefe50165b39fa24ba9237e014a6
Jan Preisig <tiepre@tie.local>
2017-01-27 13:53:13 +0100

    ferien seite zu Zeit aendern

--- subsites/ferien.php
+++ subsites/ferien.php
@@ -19,59 +19,13 @@ $nameOfUser = $_GET['user'];
 
 </head>
 <body>
+<input type="date" id="date_input">
+<input type="time" id="starttime_input">
+<input type="time" id="endtime_input">
+<button id="savetime">Save</button>
+<div id="uebersicht"></div>
 
-<div class="month">
- <ul class="pager">
-    <li class="previous"><a href="#"><span aria-hidden="true">&larr;</span>letzter Monat</a></li>
-    <li class="next"><a href="#">nächster Monat <span aria-hidden="true">&rarr;</span></a></li>
-    <li style="text-align:center">August</li>
 
-  </ul>
-</div>
-
-<ul class="weekdays">
-  <li>Mo</li>
-  <li>Tu</li>
-  <li>We</li>
-  <li>Th</li>
-  <li>Fr</li>
-  <li>Sa</li>
-  <li>Su</li>
-</ul>
-
-<ul class="days">
-  <li>1</li>
-  <li>2</li>
-  <li>3</li>
-  <li>4</li>
-  <li>5</li>
-  <li>6</li>
-  <li>7</li>
-  <li>8</li>
-  <li>9</li>
-  <li>10</li>
-  <li>11</li>
-  <li>12</li>
-  <li>13</li>
-  <li>14</li>
-  <li>15</li>
-  <li>16</li>
-  <li>17</li>
-  <li>18</li>
-  <li>19</li>
-  <li>20</li>
-  <li>21</li>
-  <li>22</li>
-  <li>23</li>
-  <li>24</li>
-  <li>25</li>
-  <li>26</li>
-  <li>27</li>
-  <li>28</li>
-  <li>29</li>
-  <li>30</li>
-  <li>31</li>
-</ul>
 </body>
 <?php  
 
