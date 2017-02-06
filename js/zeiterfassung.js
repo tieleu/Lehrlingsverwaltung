f5c48deeadbbc06e12ebedf94ceb450a7f2cc541
Jan Preisig <tiepre@tie.local>
2017-02-06 11:36:55 +0100

    edit js

--- js/zeiterfassung.js
+++ js/zeiterfassung.js
@@ -2,22 +2,20 @@ $(document).ready(function(){
 	$(starttime_input).focus();
 	console.log($(starttime_input).is(':focus'));
 
-	var $cookietime; 
+	  $(function(){
+    var intervalID;
+    var freqSecs = 1.2;
+    intervalID = setInterval (RepeatCall, freqSecs*1000 );
+
+    function RepeatCall() {
+      var inout = (freqSecs*1000)/2;
+      $("#starttime_input").fadeIn(inout).fadeOut(inout);
+    }
+
+    
+  });
 
     while ($(starttime_input).is(':focus')){
-    	$(function(){
-	    var intervalID;
-	    var freqSecs = 1.2;
-	    intervalID = setInterval (RepeatCall, freqSecs*1000 );
-
-	    function RepeatCall() {
-	      document.cookie = valueOf('#starttime_input');
-	      $cookietime = document.cookie;
-	      console.log($cookietime);
-	    }
-
-	    
-	  });
-	    	
-	    }
+    	
+    }
 });
