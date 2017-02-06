86b1ec5964b0bd20cb8c43dd21fe25ef3bcb2e9e
Jan Preisig <tiepre@tie.local>
2017-02-06 11:03:42 +0100

    edit js

--- js/zeiterfassung.js
+++ js/zeiterfassung.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
+	$(starttime_input).focus();
 	console.log($(starttime_input).is(':focus'));
+
     while ($(starttime_input).is(':active')){
     	console.log('active');
     }
