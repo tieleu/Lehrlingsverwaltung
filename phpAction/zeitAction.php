077729f1baa2a961f96d24272d4b86f04a5be74e
Jan Preisig <tiepre@tie.local>
2017-02-06 14:07:20 +0100

    add zeit action file

--- phpAction/zeitAction.php
+++ phpAction/zeitAction.php
@@ -16,20 +16,22 @@ if(isset($_POST['savetime'])){
 	$endtime = $_POST['endtime_input'];
 	$date = $_POST['date_input']; 
 	mysql_query("INSERT INTO zeit (zeit_morgen, zeit_nachmittag,endzeit,zeit_differenz) VALUES ('$starttime', '$endtime', 8,0 )", $conn);
-	$timeId = mysql_query("SELECT MAX(id) FROM zeit", $conn);
-	$userId = mysql_query("SELECT idUser FROM User WHERE username = '$user'", $conn);
+	$rowstime = mysql_query("SELECT MAX(id) FROM zeit", $conn);
+	$rowsuser = mysql_query("SELECT idUser FROM User WHERE username = '$user'", $conn);
 
-	$rowstime = mysql_num_rows($timeId);
-	$rowsuser = mysql_num_rows($userId);
+echo $user; 
+
 	$idUser = 0;
 	$idTime = 0;
+
 	while($row = mysql_fetch_object($rowsuser)){
 		$idUser = $row -> idUser;
 	}
-
+echo "userId: " . $idUser;
 	while($row1 = mysql_fetch_object($rowstime)){
 		$idTime = $row1 -> id;
 	}
+echo "timeId: " . $idTime;
 
 	mysql_query("INSERT INTO User_has_zeit (User_idUser, zeit_id) VALUES ($idUser,$idTime)");
 }
