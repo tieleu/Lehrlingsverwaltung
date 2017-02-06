db18e37f1e1ed477e147b586618d2b67ca6c450c
Jan Preisig <tiepre@tie.local>
2017-02-06 10:38:17 +0100

    create zeit js and include it

--- js/zeiterfassung.js
+++ js/zeiterfassung.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
-    $(starttime_input).focus(function(){
-    	var $span = "testtest";
-        $span.css("display", "inline").fadeOut(2000);
+    $("input").click(function(){
+    	$(this).hide();
     });
 });
