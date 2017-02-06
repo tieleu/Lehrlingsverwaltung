cb91da60f33bf7587bff350b8530b8609c88343a
Jan Preisig <tiepre@tie.local>
2017-02-06 15:49:48 +0100

    add zeit action file

--- phpAction/zeitAction.php
+++ phpAction/zeitAction.php
@@ -3,13 +3,6 @@ session_start();
 include('../subsites/header.php');
 $user = $_GET['user'];
 
-$servername = "172.16.44.5";
-$username = "lehrling";
-$password = "sec1.01";
-
-$conn =mysql_connect($servername, $username, $password) 	
-or die("Fehler im System");
-
 mysql_select_db("Lehrverwaltung");
 if(isset($_POST['savetime'])){
 	$starttime = $_POST['starttime_input'];
@@ -24,23 +17,22 @@ if(isset($_POST['savetime'])){
 	$worktime = $endtime - $starttime;
 	echo $worktime;
 
-	mysql_query("INSERT INTO zeit (zeit_morgen, zeit_nachmittag,endzeit,zeit_differenz, date) VALUES ('$starttime', '$endtime', 8,0, '$date' )", $conn);
+	$check = mysql_query("SELECT User_has_zeit.User_idUser, User_has_zeit.zeit_id, zeit.date, zeit.id From User_has_zeit JOIN zeit ON User_has_zeit.zeit_id=zeit.id WHERE zeit.date='$date' AND User_has_zeit.User_idUser=$idUser;");
+	if(mysql_num_rows($check)!=1){
+
+
+	mysql_query("INSERT INTO zeit (zeit_morgen, date) VALUES ('$worktime', '$date' )", $conn);
 	$rowstime = mysql_query("SELECT MAX(id) AS id FROM zeit", $conn);
-
-	
-
-
 	echo $user; 
 
 	$idTime = 0;
-
 
 	while($row1 = mysql_fetch_object($rowstime)){
 		$idTime = $row1 -> id;
 	}
 	echo "timeId: " . $idTime;
-
 	mysql_query("INSERT INTO User_has_zeit (User_idUser, zeit_id) VALUES ($idUser,$idTime)", $conn);
 }
-#header("Location: ../subsites/zeiterfassung.php?user=$user");
+}
+	header("Location: ../subsites/zeiterfassung.php?user=$user");
 ?>
