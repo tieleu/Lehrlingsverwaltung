7349b2839f838835f467e7010bb7b6cca2621ce5
Jan Preisig <tiepre@tie.local>
2017-02-07 10:08:30 +0100

    minuten wieder in zeit umrechnen

--- subsites/zeiterfassung.php
+++ subsites/zeiterfassung.php
@@ -22,24 +22,23 @@ $user = $_GET['user'];
   </tr>
 		<div id="zeile">
 			<?php
-				function minToTime($time){
-					$rest = $time%60;
-					$hours = ($time-$rest)/60;
-					return $hours . ":" . $rest;
-				} 
-				$idUser;
-				$getId = mysql_query("SELECT idUser FROM User WHERE username = '$user'", $conn);
-				while($row = mysql_fetch_object($getId)){
-					$idUser = $row -> idUser;
-				}
-				$getContent = mysql_query("SELECT User_has_zeit.User_idUser, zeit.date, zeit.endzeit, zeit.zeit_differenz FROM User_has_zeit JOIN zeit ON User_has_zeit.zeit_id=zeit.id WHERE User_has_zeit.User_idUser=$idUser", $conn);
-				while($row1 = mysql_fetch_object($getContent)){
-					$date = $row1 -> date;
-					$zeittotal = minToTime($row1 -> endzeit);
-					$differenz = minToTime($row1 -> zeit_differenz);
-					echo "<tr id='zeile'><td><input type='text' class='form-control' placeholder='Datum' value='$date' readonly></td><td><input type='text' class='form-control' placeholder='erreichte Zeit' value='$zeittotal' readonly></td><td><input type='text' class='form-control' placeholder='Sollzeit' value='8:24 h' readonly></td><td><input type='text' class='form-control' placeholder='Differenz Zeit' value='$differenz' readonly></td></tr>";
-
-				}
+			function minToTime($time){
+				$rest = $time%60;
+				$hours = ($time-$rest)/60;
+				return $hours . ":" . $rest;
+			} 
+			$idUser;
+			$getId = mysql_query("SELECT idUser FROM User WHERE username = '$user'", $conn);
+			while($row = mysql_fetch_object($getId)){
+				$idUser = $row -> idUser;
+			}
+			$getContent = mysql_query("SELECT User_has_zeit.User_idUser, zeit.date, zeit.endzeit, zeit.zeit_differenz FROM User_has_zeit JOIN zeit ON User_has_zeit.zeit_id=zeit.id WHERE User_has_zeit.User_idUser=$idUser", $conn);
+			while($row1 = mysql_fetch_object($getContent)){
+				$date = $row1 -> date;
+				$zeittotal = minToTime($row1 -> endzeit);
+				$differenz = minToTime($row1 -> zeit_differenz);
+				echo "<tr id='zeile'><td><input type='text' class='form-control' placeholder='Datum' value='$date' readonly></td><td><input type='text' class='form-control' placeholder='erreichte Zeit' value='$zeittotal' readonly></td><td><input type='text' class='form-control' placeholder='Sollzeit' value='8:24 h' readonly></td><td><input type='text' class='form-control' placeholder='Differenz Zeit' value='$differenz' readonly></td></tr>";
+			}
 			?>
 			<tr id="zeile">
 			    <td><input type="text" class="form-control" placeholder="Datum" readonly></td>
