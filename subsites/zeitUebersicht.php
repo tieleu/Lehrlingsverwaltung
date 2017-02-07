bf5f63e3410adcfbb58fb3d59c8b4c0b9280249a
Jan Preisig <tiepre@tie.local>
2017-02-07 13:32:38 +0100

    zeit übersicht für thomas

--- subsites/zeitUebersicht.php
+++ subsites/zeitUebersicht.php
@@ -109,6 +109,7 @@ $user = $_GET['user'];
 				if (isset($_POST['select'])) {
 					$chosenUser = $_POST['select'];
 					$getContent = mysql_query("SELECT User_has_zeit.User_idUser, zeit.date, zeit.endzeit, zeit.zeit_differenz FROM User_has_zeit JOIN zeit ON User_has_zeit.zeit_id=zeit.id WHERE User_has_zeit.User_idUser=$chosenUser", $conn);
+					echo $chosenUser;
 					while($row1 = mysql_fetch_object($getContent)){
 						$date = $row1 -> date;
 						$zeittotal = minToTime($row1 -> endzeit);
