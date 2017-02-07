2d95a35dbe8cbab16a3ef4af768f1216425ca419
Jan Preisig <tiepre@tie.local>
2017-02-07 11:05:55 +0100

    zeit übersicht für thomas

--- index.php
+++ index.php
@@ -70,7 +70,13 @@ $conn =mysql_connect($servername, $username, $password)
 				        </ul>
 				      </li>
 							<li><a href="subsites/uebungen.php?user=<?php echo $nameOfUser ?>">Übungen</a></li>
-							<li><a href="subsites/zeiterfassung.php?user=<?php echo $nameOfUser ?>">Zeiterfassung</a></li> 
+							<?php 
+							if($nameOfUser=="tiefri"){
+							echo "<li><a href='subsites/zeitUebersicht.php?user=<?php echo $nameOfUser ?>'>Zeiterfassung Übersicht</a></li>";	
+							}else{
+							echo "<li><a href='subsites/zeiterfassung.php?user=<?php echo $nameOfUser ?>'>Zeiterfassung</a></li>";
+							}
+							?> 
 							<li><a href="subsites/notendossier.php?user=<?php echo $nameOfUser ?>">Notendossier</a></li>  
 						</ul>
 						<ul class="nav navbar-nav navbar-right">
