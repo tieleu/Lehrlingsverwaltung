eedea2e64e36b4f2b50a55b33de160b0362adb11
Jan Preisig <tiepre@tie.local>
2017-02-07 14:16:47 +0100

    zeit übersicht für thomas

--- subsites/zeitUebersicht.php
+++ subsites/zeitUebersicht.php
@@ -12,16 +12,7 @@ $user = $_GET['user'];
 	<title>Lehrverwaltung - Lehrplan</title>
 </head>
 <body>
-				<div class="panel-heading">
-			
-			<form action="" method="post">
-				<select id="select" name="select" onchange="this.form.submit()" style="width: 170px; height: 35px;">
-			<option value="1">Ruben</option>
-			<option value="2">Luca</option>
-			<option value="3">Jan</option>
-						</select>
-					</form>					
-				</div>
+				
 
 <div id="uebersicht">
    <table class="table">
@@ -31,19 +22,7 @@ $user = $_GET['user'];
     <th>Sollzeit</th>
     <th>Differenz</th>
   </tr>
-		<?php
-				if(isset($_POST['select'])){
-					$idofChosen = $_POST['select'];
-					$getContent = mysql_query("SELECT User_has_zeit.User_idUser, zeit.date, zeit.endzeit, zeit.zeit_differenz FROM User_has_zeit JOIN zeit ON User_has_zeit.zeit_id=zeit.id WHERE User_has_zeit.User_idUser='$idofChosen'", $conn);
-					echo $idofChosen;
-			while($row1 = mysql_fetch_object($getContent)){
-				$date = $row1 -> date;
-				$zeittotal = minToTime($row1 -> endzeit);
-				$differenz = minToTime($row1 -> zeit_differenz);
-				?><tr id='zeile'><td><input type='text' class='form-control' placeholder='Datum' value='$date' readonly></td><td><input type='text' class='form-control' placeholder='erreichte Zeit' value='$zeittotal' readonly></td><td><input type='text' class='form-control' placeholder='Sollzeit' value='8:24 h' readonly></td><td><input type='text' class='form-control' placeholder='Differenz Zeit' value='$differenz' readonly></td></tr><?php
-				}
-			}
-			?>
+		
 						
 	</table>
 </div>
