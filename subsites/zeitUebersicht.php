bcb0cba1b4450b26c2bf8d60182031d34e2fc0b1
Jan Preisig <tiepre@tie.local>
2017-02-07 13:36:03 +0100

    zeit übersicht für thomas

--- subsites/zeitUebersicht.php
+++ subsites/zeitUebersicht.php
@@ -44,6 +44,19 @@ $user = $_GET['user'];
 				$zeittotal = minToTime($row1 -> endzeit);
 				$differenz = minToTime($row1 -> zeit_differenz);
 			}*/
+				
+				if (isset($_POST['select'])) {
+					$chosenUser = $_POST['select'];
+					$getContent = mysql_query("SELECT User_has_zeit.User_idUser, zeit.date, zeit.endzeit, zeit.zeit_differenz FROM User_has_zeit JOIN zeit ON User_has_zeit.zeit_id=zeit.id WHERE User_has_zeit.User_idUser=$chosenUser", $conn);
+					while($row1 = mysql_fetch_object($getContent)){
+						$date = $row1 -> date;
+						$zeittotal = minToTime($row1 -> endzeit);
+						$differenz = minToTime($row1 -> zeit_differenz);
+					echo $chosenUser;
+						echo "<tr id='zeile'><td><input type='text' class='form-control' placeholder='Datum' value='$date' readonly></td><td><input type='text' class='form-control' placeholder='erreichte Zeit' value='$zeittotal' readonly></td><td><input type='text' class='form-control' placeholder='Sollzeit' value='8:24 h' readonly></td><td><input type='text' class='form-control' placeholder='Differenz Zeit' value='$differenz' readonly></td></tr>";
+					}
+				}
+				
 			?>
 			<tr id='zeile'>
 			<td><input type='text' class='form-control' placeholder='Datum' value='$date' readonly></td>
@@ -105,19 +118,6 @@ $user = $_GET['user'];
 						</select>
 					</form>					
 				</div>
-				<?php
-				if (isset($_POST['select'])) {
-					$chosenUser = $_POST['select'];
-					$getContent = mysql_query("SELECT User_has_zeit.User_idUser, zeit.date, zeit.endzeit, zeit.zeit_differenz FROM User_has_zeit JOIN zeit ON User_has_zeit.zeit_id=zeit.id WHERE User_has_zeit.User_idUser=$chosenUser", $conn);
-					while($row1 = mysql_fetch_object($getContent)){
-						$date = $row1 -> date;
-						$zeittotal = minToTime($row1 -> endzeit);
-						$differenz = minToTime($row1 -> zeit_differenz);
-					echo $chosenUser;
-						echo "<tr id='zeile'><td><input type='text' class='form-control' placeholder='Datum' value='$date' readonly></td><td><input type='text' class='form-control' placeholder='erreichte Zeit' value='$zeittotal' readonly></td><td><input type='text' class='form-control' placeholder='Sollzeit' value='8:24 h' readonly></td><td><input type='text' class='form-control' placeholder='Differenz Zeit' value='$differenz' readonly></td></tr>";
-					}
-				}
-				?>
 				
 
 	
