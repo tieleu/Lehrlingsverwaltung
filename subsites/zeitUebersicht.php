813d5b5557227c019d35a4ae86cdb9c4d7cdb93e
Jan Preisig <tiepre@tie.local>
2017-02-07 14:05:12 +0100

    zeit übersicht für thomas

--- subsites/zeitUebersicht.php
+++ subsites/zeitUebersicht.php
@@ -13,15 +13,12 @@ $user = $_GET['user'];
 </head>
 <body>
 				<div class="panel-heading">
-			<?php
-			$ausgabe = "SELECT vorname, username, idUser from User where status = 'lehrling'";
-			$ergebniss = mysql_query($ausgabe);
-			?>
+			
 			<form action="" method="post">
 				<select id="select" name="select" onchange="this.form.submit()" style="width: 170px; height: 35px;">
 			<option value="1">Ruben</option>
 			<option value="2">Luca</option>
-
+			<option value="3">Jan</option>
 						</select>
 					</form>					
 				</div>
@@ -35,19 +32,15 @@ $user = $_GET['user'];
     <th>Differenz</th>
   </tr>
 		<?php
-					$ausgabe = "SELECT * from Aufgaben where User_idUser=$userID";
-					$ergebniss = mysql_query($ausgabe);
-
-					while ($row = mysql_fetch_object($ergebniss)) {
-						$prio = $row-> prioritaet;
-						$id = $row -> idAufgaben;
-					}
-					$getContent = mysql_query("SELECT User_has_zeit.User_idUser, zeit.date, zeit.endzeit, zeit.zeit_differenz FROM User_has_zeit JOIN zeit ON User_has_zeit.zeit_id=zeit.id WHERE User_has_zeit.User_idUser=$selectOption", $conn);
+				if(isset($_POST['select'])){
+					$idofChosen = $_POST['select'];
+					$getContent = mysql_query("SELECT User_has_zeit.User_idUser, zeit.date, zeit.endzeit, zeit.zeit_differenz FROM User_has_zeit JOIN zeit ON User_has_zeit.zeit_id=zeit.id WHERE User_has_zeit.User_idUser=$idofChosen", $conn);
 			while($row1 = mysql_fetch_object($getContent)){
 				$date = $row1 -> date;
 				$zeittotal = minToTime($row1 -> endzeit);
 				$differenz = minToTime($row1 -> zeit_differenz);
 				echo "<tr id='zeile'><td><input type='text' class='form-control' placeholder='Datum' value='$date' readonly></td><td><input type='text' class='form-control' placeholder='erreichte Zeit' value='$zeittotal' readonly></td><td><input type='text' class='form-control' placeholder='Sollzeit' value='8:24 h' readonly></td><td><input type='text' class='form-control' placeholder='Differenz Zeit' value='$differenz' readonly></td></tr>";
+				}
 			}
 			?>
 						
