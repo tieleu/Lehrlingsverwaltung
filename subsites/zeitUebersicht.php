c42cb0c2aaf92f27c7718d7b4219439e5ec36396
Jan Preisig <tiepre@tie.local>
2017-02-07 15:11:43 +0100

    css

--- subsites/zeitUebersicht.php
+++ subsites/zeitUebersicht.php
@@ -28,10 +28,10 @@ $user = $_GET['user'];
 						  </tr>
 						<?php
 						$getContent = mysql_query("SELECT User_has_zeit.User_idUser, zeit.date, zeit.endzeit, zeit.zeit_differenz FROM User_has_zeit JOIN zeit ON User_has_zeit.zeit_id=zeit.id WHERE User_has_zeit.User_idUser=1", $conn);
-						while($row1 = mysql_fetch_object($getContent)){
-							$date = $row1 -> date;
-							$zeittotal = minToTime($row1 -> endzeit);
-							$differenz = minToTime($row1 -> zeit_differenz);
+						while($row = mysql_fetch_object($getContent)){
+							$date = $row -> date;
+							$zeittotal = minToTime($row -> endzeit);
+							$differenz = minToTime($row -> zeit_differenz);
 							echo "<tr id='zeile'><td><input type='text' class='form-control' placeholder='Datum' value='$date' readonly></td><td><input type='text' class='form-control' placeholder='erreichte Zeit' value='$zeittotal' readonly></td><td><input type='text' class='form-control' placeholder='Sollzeit' value='8:24 h' readonly></td><td><input type='text' class='form-control' placeholder='Differenz Zeit' value='$differenz' readonly></td></tr>";
 						}
 						?>
@@ -49,12 +49,12 @@ $user = $_GET['user'];
 							<th>Differenz</th>
 						  </tr>
 						<?php
-						$getContent = mysql_query("SELECT User_has_zeit.User_idUser, zeit.date, zeit.endzeit, zeit.zeit_differenz FROM User_has_zeit JOIN zeit ON User_has_zeit.zeit_id=zeit.id WHERE User_has_zeit.User_idUser=2", $conn);
-						while($row1 = mysql_fetch_object($getContent)){
-							$date = $row1 -> date;
-							$zeittotal = minToTime($row1 -> endzeit);
-							$differenz = minToTime($row1 -> zeit_differenz);
-							echo "<tr id='zeile'><td><input type='text' class='form-control' placeholder='Datum' value='$date' readonly></td><td><input type='text' class='form-control' placeholder='erreichte Zeit' value='$zeittotal' readonly></td><td><input type='text' class='form-control' placeholder='Sollzeit' value='8:24 h' readonly></td><td><input type='text' class='form-control' placeholder='Differenz Zeit' value='$differenz' readonly></td></tr>";
+						$getContent1 = mysql_query("SELECT User_has_zeit.User_idUser, zeit.date, zeit.endzeit, zeit.zeit_differenz FROM User_has_zeit JOIN zeit ON User_has_zeit.zeit_id=zeit.id WHERE User_has_zeit.User_idUser=2", $conn);
+						while($row1 = mysql_fetch_object($getContent1)){
+							$date1 = $row1 -> date;
+							$zeittotal1 = minToTime($row1 -> endzeit);
+							$differenz1 = minToTime($row1 -> zeit_differenz);
+							echo "<tr id='zeile'><td><input type='text' class='form-control' placeholder='Datum' value='$date1' readonly></td><td><input type='text' class='form-control' placeholder='erreichte Zeit' value='$zeittotal1' readonly></td><td><input type='text' class='form-control' placeholder='Sollzeit' value='8:24 h' readonly></td><td><input type='text' class='form-control' placeholder='Differenz Zeit' value='$differenz1' readonly></td></tr>";
 						}
 						?>
 						
@@ -71,12 +71,12 @@ $user = $_GET['user'];
 							<th>Differenz</th>
 						  </tr>
 						<?php
-						$getContent = mysql_query("SELECT User_has_zeit.User_idUser, zeit.date, zeit.endzeit, zeit.zeit_differenz FROM User_has_zeit JOIN zeit ON User_has_zeit.zeit_id=zeit.id WHERE User_has_zeit.User_idUser=3", $conn);
-						while($row1 = mysql_fetch_object($getContent)){
-							$date = $row1 -> date;
-							$zeittotal = minToTime($row1 -> endzeit);
-							$differenz = minToTime($row1 -> zeit_differenz);
-							echo "<tr id='zeile'><td><input type='text' class='form-control' placeholder='Datum' value='$date' readonly></td><td><input type='text' class='form-control' placeholder='erreichte Zeit' value='$zeittotal' readonly></td><td><input type='text' class='form-control' placeholder='Sollzeit' value='8:24 h' readonly></td><td><input type='text' class='form-control' placeholder='Differenz Zeit' value='$differenz' readonly></td></tr>";
+						$getContent2 = mysql_query("SELECT User_has_zeit.User_idUser, zeit.date, zeit.endzeit, zeit.zeit_differenz FROM User_has_zeit JOIN zeit ON User_has_zeit.zeit_id=zeit.id WHERE User_has_zeit.User_idUser=3", $conn);
+						while($row2 = mysql_fetch_object($getContent2)){
+							$date2 = $row2 -> date;
+							$zeittotal2 = minToTime($row2 -> endzeit);
+							$differenz2 = minToTime($row2 -> zeit_differenz);
+							echo "<tr id='zeile'><td><input type='text' class='form-control' placeholder='Datum' value='$date2' readonly></td><td><input type='text' class='form-control' placeholder='erreichte Zeit' value='$zeittotal2' readonly></td><td><input type='text' class='form-control' placeholder='Sollzeit' value='8:24 h' readonly></td><td><input type='text' class='form-control' placeholder='Differenz Zeit' value='$differenz2' readonly></td></tr>";
 						}
 						?>
 						
