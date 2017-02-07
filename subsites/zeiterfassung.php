5ce8ca4b7d2a4fe81377fdc4e356dacdb09c226d
Jan Preisig <tiepre@tie.local>
2017-02-07 10:20:21 +0100

    date default value

--- subsites/zeiterfassung.php
+++ subsites/zeiterfassung.php
@@ -40,12 +40,6 @@ $user = $_GET['user'];
 				echo "<tr id='zeile'><td><input type='text' class='form-control' placeholder='Datum' value='$date' readonly></td><td><input type='text' class='form-control' placeholder='erreichte Zeit' value='$zeittotal' readonly></td><td><input type='text' class='form-control' placeholder='Sollzeit' value='8:24 h' readonly></td><td><input type='text' class='form-control' placeholder='Differenz Zeit' value='$differenz' readonly></td></tr>";
 			}
 			?>
-			<tr id="zeile">
-			    <td><input type="text" class="form-control" placeholder="Datum" readonly></td>
-			    <td><input type="text" class="form-control" placeholder="erreichte Zeit" readonly></td>
-			    <td><input type="text" class="form-control" placeholder="Sollzeit" value="8.4 h" readonly></td>
-			    <td><input type="text" class="form-control" placeholder="Differenz Zeit" readonly></td>
-			</tr>
 		</div>
 	</table>
 </div>
