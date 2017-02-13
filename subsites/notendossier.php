cf3b591d44cd33d541ff576402be6ff8c9cb8e61
Jan Preisig <tiepre@tie.local>
2017-02-13 13:10:46 +0100

    notendossier edit

--- subsites/notendossier.php
+++ subsites/notendossier.php
@@ -8,59 +8,61 @@
 	$password = "sec1.01";
 
 	$conn = mysql_connect($servername, $username, $password)
-		or die("Fehler im System");
-		mysql_select_db("Lehrverwaltung");
+	or die("Fehler im System");
+	mysql_select_db("Lehrverwaltung");
 	$select ="SELECT * from Noten where User_idUser=1";
 	?>
 	<link rel="stylesheet" type="text/css" href="../css/noten.css">
 	<script type="text/javascript" href="../jquery/jquery-3.1.1.js"></script>
 	<script  type="text/javascript" src="../js/notendossier.js" defer></script>
-  
+
 
 	<title>Lehrverwaltung - Noten Dossier</title>
 
 </head>
 <body>
-	<div id="container1">
+	<div id="uebersicht">
 		<h1>Notendossier</h1>
-		    <div class="panel" id="scroll">
-				<ul class="nav nav-tabs">
-					<li role="presentation"><a href="#">Lehrjahr1 Sem.1</a></li>
-					<li role="presentation"><a href="#">Lehrjahr1 Sem.2</a></li>
-					<li role="presentation"><a href="#">Lehrjahr2 Sem.1</a></li>
-					<li role="presentation"><a href="#">Lehrjahr2 Sem.2</a></li>
-					<li role="presentation"><a href="#">Lehrjahr3 Sem.1</a></li>
-					<li role="presentation"><a href="#">Lehrjahr3 Sem.2</a></li>
-					<li role="presentation"><a href="#">Lehrjahr4 Sem.1</a></li>
-					<li role="presentation"><a href="#">Lehrjahr4 Sem.2</a></li>
-				</ul>
-				<br>
+		<div class="panel" id="scroll">
+			<ul class="nav nav-tabs">
+				<li role="presentation"><a href="#">Lehrjahr1 Sem.1</a></li>
+				<li role="presentation"><a href="#">Lehrjahr1 Sem.2</a></li>
+				<li role="presentation"><a href="#">Lehrjahr2 Sem.1</a></li>
+				<li role="presentation"><a href="#">Lehrjahr2 Sem.2</a></li>
+				<li role="presentation"><a href="#">Lehrjahr3 Sem.1</a></li>
+				<li role="presentation"><a href="#">Lehrjahr3 Sem.2</a></li>
+				<li role="presentation"><a href="#">Lehrjahr4 Sem.1</a></li>
+				<li role="presentation"><a href="#">Lehrjahr4 Sem.2</a></li>
+			</ul>
+			<br>
 
-				</div>
-  		<form action="" method="post">
-  		<select name="schulfachselect">
-  			<?php
-  				$faecher = mysql_query("SELECT * FROM Schulfach");
-  				while ($row = mysql_fetch_object($faecher)) {
-  					$fach = $row -> Name;
-  					$id = $row -> idSchulfach;
-  				
-  			?>
-  			<option value="<?php echo $id; ?>"><?php echo $fach; ?></option>
-  			<?php } ?>
-  		</select><label for="grade">Note:</label><input type="number" name="grade" placeholder="6" min="1" max="6" style="width: 50px;">
-  		<button name="savetest" id="Eingabe">save</button>
-  		</form>
-  		<?php
-  		if (isset($_POST['savetest'])) {
-  			$test = $_POST['schulfachselect'];
-  			echo $test;
-  		}
-  		?>
-    		</div>
-	<footer id="footer">
-</footer>
+		</div>
+		<div id="input_container">
+			<form action="" method="post">
+				<select name="schulfachselect">
+					<?php
+					$faecher = mysql_query("SELECT * FROM Schulfach");
+					while ($row = mysql_fetch_object($faecher)) {
+						$fach = $row -> Name;
+						$id = $row -> idSchulfach;
+
+						?>
+						<option value="<?php echo $id; ?>"><?php echo $fach; ?></option>
+						<?php } ?>
+					</select><label for="grade">Note:</label><input type="number" name="grade" placeholder="6" min="1" max="6" style="width: 50px;">
+					<button name="savetest" id="Eingabe">save</button>
+				</form>
+				<?php
+				if (isset($_POST['savetest'])) {
+					$test = $_POST['schulfachselect'];
+					echo $test;
+				}
+				?>
+			</div>
+		</div>
+		<footer id="footer">
+		</footer>
 
 
-</body>
-</html>
+	</body>
+	</html>
