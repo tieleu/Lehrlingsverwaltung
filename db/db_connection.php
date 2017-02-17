3d45a0d47352dc1beea099a8fff1adc18d31f68a
ruben leuenberger <ruben.leuenberger@tie.ch>
2017-02-17 09:46:49 +0100

    edit code

---
+++ db/db_connection.php
@@ -0,0 +1,11 @@
+<?php  
+
+$servername = "172.16.44.5";
+$username = "lehrling";
+$password = "sec1.01";
+
+$conn =mysql_connect($servername, $username, $password)
+	or die("Fehler im System");
+	mysql_select_db("Lehrverwaltung");
+mysql_query("SET NAMES 'utf8'",$conn);
+?>
