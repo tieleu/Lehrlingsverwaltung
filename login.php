8977b9fce18bfbed3661da620f84a0156580e9ae
ruben leuenberger <ruben.leuenberger@tie.ch>
2017-02-17 10:30:37 +0100

    edit code

--- login.php
+++ login.php
@@ -12,12 +12,12 @@
 
 	    $_username = mysql_real_escape_string($_POST["username"]); 
 	    $_passwort = mysql_real_escape_string($_POST["passwort"]); 
-	
+	echo "thas: " . $_username;
 
 		require("db/db_connection.php");
 		$query = mysql_query("select username, passwort from User where passwort='$password' AND username='$username'");
 		$rows = mysql_num_rows($query);
-echo $rows;
+
 		if ($rows == 1) {
 			$_SESSION['login_user']=$username; 
          	$_SESSION['eingeloggt'] = true;
