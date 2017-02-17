b6078076b2b2728a0cce8dbdac10a664b7c216a7
ruben leuenberger <ruben.leuenberger@tie.ch>
2017-02-17 10:13:35 +0100

    edit code

--- login.php
+++ login.php
@@ -1,11 +1,12 @@
 <?php
+include("db/db_connection.php");
 	      session_start();
   session_cache_limiter(3600);
 	if (empty($_POST['username']) || empty($_POST['password'])) {
 		$error = "Username or Password is invalid";	
 	}
 	else{
-		include("db/db_connection.php");
+		
 		$query = mysql_query("select username, passwort from User where passwort='$password' AND username='$username'");
 		$rows = mysql_num_rows($query);
 
@@ -17,7 +18,7 @@
 		} else {
 			$error = "Username or Password is invalid";
 			$_SESSION['eingeloggt'] = false;
-			header("Location: login.html");
+		#	header("Location: login.html");
 		}
 	}
 ?>
