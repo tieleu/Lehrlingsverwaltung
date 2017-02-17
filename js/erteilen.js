257398f7a9f2c08927ed1c5f24fb1ac8ffdcc15a
Jan Preisig <tiepre@tie.local>
2017-02-17 15:40:23 +0100

    message

---
+++ js/erteilen.js
@@ -0,0 +1,19 @@
+alert(getCookie("erteilen_Status"));
+document.cookie = "erteilen_Status=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
