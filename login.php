03e432fa62c04985d53dfb500df947acb8b14b99
ruben leuenberger <ruben.leuenberger@tie.ch>
2017-02-17 10:08:39 +0100

    edit code

--- login.php
+++ login.php
@@ -5,25 +5,15 @@
 		$error = "Username or Password is invalid";	
 	}
 	else{
-		$username=$_POST['username'];
-		$password=$_POST['password'];
-
-	    $_username = mysql_real_escape_string($_POST["username"]); 
-	    $_passwort = mysql_real_escape_string($_POST["passwort"]); 
-
-		$connection = mysql_connect("172.16.44.5", "lehrling", "sec1.01");
-
+		include("../db/db_connection.php");
 		$db = mysql_select_db("Lehrverwaltung", $connection);
-
 		$query = mysql_query("select username, passwort from User where passwort='$password' AND username='$username'", $connection);
-
 		$rows = mysql_num_rows($query);
+
 		if ($rows == 1) {
 			$_SESSION['login_user']=$username; 
          	$_SESSION['eingeloggt'] = true;
-			#setcookie('username', $username,time() + (10 * 365 * 24 * 60 * 60));
 			header("location:index.php?user=$username"); 
-
 
 		} else {
 			$error = "Username or Password is invalid";
