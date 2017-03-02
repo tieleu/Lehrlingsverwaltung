75fef0ae1fb50efcfa466dab83cc46e94897c27d
Jan Preisig <tiepre@tie.local>
2017-03-02 13:51:21 +0100

    zeituebersicht

--- phpAction/zeitUebersichtAction.php
+++ phpAction/zeitUebersichtAction.php
@@ -2,6 +2,11 @@
 include('../subsites/header.php');
 $user = $_GET['user'];
 $id = $_POST['id'];
-echo $id;
+$startzeit = $_POST['date']." ".$_POST['starttime'];
+mysql_query("INSERT INTO zeit (user_id, zeit) VALUES ($id, $startzeit)");
+$endzeit = $_POST['date']." ".$_POST['endtime'];
+mysql_query("INSERT INTO zeit (user_id, zeit) VALUES ($id, $endzeit)");
+header("Location: ../subsites/zeitUebersicht.php");
+
 
 ?>
