f5f1bfd656b3197d4c8347849ad125f50a8b8e20
Jan Preisig <tiepre@tie.local>
2017-03-03 13:44:58 +0100

    edit settings

--- subsites/einstellungen.php
+++ subsites/einstellungen.php
@@ -17,7 +17,7 @@ $user = $_GET['user'];
     <div id="userOptions">
     	<form action="../phpAction/changePasswordAction.php?user=<?php echo $nameOfUser ?>" method="post">
         <div id="passwortChange">
-            <h2>Passwort ändern</h2>
+            <h2 align="center">Passwort ändern</h2>
             <label for="old" class="labelforPW">Altes Passwort <input class="form-control inputPW" type="password" name="old" value="" placeholder="altes Passwort" required="true"></label>
             <label for="new1" class="labelforPW">Neues Passwort <input class="form-control inputPW" type="password" name="new1" value="" placeholder="neues Passwort" required="true"></label>
             <label for="new2" class="labelforPW">Passwort bestätigen <input class="form-control inputPW" type="password" name="new2" value="" placeholder="Passwort bestätigen" required="true">
