b9b9be2ae4792c885eb9908c08aaef56cd9e3dd8
Jan Preisig <tiepre@tie.local>
2017-03-03 12:54:58 +0100

    keepalive session

--- index.php
+++ index.php
@@ -31,6 +31,8 @@ if($_SESSION['eingeloggt']== true && $nameOfUser == $sessionUser){
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 		<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 
+		<script type="text/javascript" src="../js/keepalive.js"></script>
+
 		<title>Lehrlingsverwaltung - Home</title>
 	</head>
 	<body>
