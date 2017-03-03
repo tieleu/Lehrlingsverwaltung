5aabd38ed24f63dcc34043a0267695b9b939f69b
Jan Preisig <tiepre@tie.local>
2017-03-03 10:20:44 +0100

    einstellungen seite erstellt und passwörter als hash verschlüsselt

---
+++ subsites/settings.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<?php 
+include("header.php");
+$user = $_GET['user'];
+?>
+<head>
+	<script type="text/javascript" href="../jquery/jquery-3.1.1.js"></script>
+	<title>Lehrlingsverwaltung - Einstellungen</title>
+</head>
+<body>
+
+</body>
+</html>
