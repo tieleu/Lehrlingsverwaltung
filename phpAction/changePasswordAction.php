cd6cad0ed133b700392b0569556b79ea4f6292ac
Jan Preisig <tiepre@tie.local>
2017-03-03 11:40:32 +0100

    edit settings

--- phpAction/changePasswordAction.php
+++ phpAction/changePasswordAction.php
@@ -6,11 +6,11 @@ $oldpw = hash("sha256", $_POST['old']);
 $newpw ="";
 if($_POST['new1']===$_POST['new2'] && $_POST['new1']!="" && $_POST['new1']!=null){
 	$newpw = hash("sha256", $_POST['new1']);
-}
 $getPWnow = mysql_query("SELECT passwort FROM User WHERE idUser=$idUser");
 $pwNow = mysql_fetch_object($getPWnow) -> passwort;
 if($oldpw===$pwNow){
 	mysql_query("UPDATE User SET passwort='$newpw' WHERE idUser=$idUser");
+}
 }
 header("Location: ../subsites/einstellungen.php?user=$user");
 ?>
