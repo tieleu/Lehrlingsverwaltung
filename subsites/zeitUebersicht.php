48b7946a905c1afa669b99c1e56ef0cc9e08900c
Jan Preisig <tiepre@tie.local>
2017-03-03 08:52:15 +0100

    zeitAction fertig

--- subsites/zeitUebersicht.php
+++ subsites/zeitUebersicht.php
@@ -158,7 +158,7 @@ echo "<tr><td></td><td></td><td></td><td></td><td><input type='text' class='form
 	?>
 		</div>
 	</table>
-	<div align="center" id="saveAndDelete">
+	<div align="center">
 		<table style="margin-bottom: 10px;">
 			<tr>
 				<th>Datum</th>
