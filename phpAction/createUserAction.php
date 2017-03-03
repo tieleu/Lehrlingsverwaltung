d3d1604aca8fd73bb9a9a9b81d99dda26b9a780a
Jan Preisig <tiepre@tie.local>
2017-03-03 15:39:45 +0100

    edit settings

--- phpAction/createUserAction.php
+++ phpAction/createUserAction.php
@@ -9,7 +9,7 @@ $status = $_POST['status'];
 $passwort ="";
 if($_POST['new1']===$_POST['new2']){
 	$passwort = hash("sha256", $_POST['new1']);
-	mysql_query("INSERT INTO User (name, vorname, username, passwort, status) VALUES ($nachname, $vorname, $username, $passwort, $status");
+	mysql_query("INSERT INTO User (name, vorname, username, passwort, status) VALUES ($nachname, $vorname, $username, $passwort, $status)");
 }
 header("Location: ../subsites/einstellungen.php?user=$user");
 
