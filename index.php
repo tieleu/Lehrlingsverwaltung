428caf16a2f1441db76028aa5d019025387bb3ea
Jan Preisig <tiepre@tie.local>
2017-03-06 10:52:20 +0100

    mainfunctions js erstellt

--- index.php
+++ index.php
@@ -31,7 +31,7 @@ if($_SESSION['eingeloggt']== true && $nameOfUser == $sessionUser){
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 		<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 
-		<script type="text/javascript" src="../js/keepalive.js"></script>
+		<script type="text/javascript" src="js/keepalive.js"></script>
 
 		<title>Lehrlingsverwaltung - Home</title>
 	</head>
