075344f1741138f38bafc4290ec7ff646f68c4f0
Jan Preisig <tiepre@tie.local>
2017-03-06 10:09:30 +0100

    mainfunctions js erstellt

--- js/zeitUebersicht.js
+++ js/zeitUebersicht.js
@@ -2,5 +2,6 @@
 	console.log("zeitUebersicht.js");
 	console.log(getCookie("delTimeStatus"));
 	if(getCookie("delTimeStatus")===1){
+		console.log(getCookie("delTimeMessage"));
 		alert(getCookie("delTimeMessage"));
 	}
