0dc18754717ee84acf90ab1f12c27fcc77dd5a7f
Jan Preisig <tiepre@tie.local>
2017-03-08 12:44:38 +0100

    create Uebung

--- subsites/uebungen.php
+++ subsites/uebungen.php
@@ -5,7 +5,7 @@
             include("header.php");
          ?>
    
-  
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/smoothness/jquery-ui.css">
      <link rel="stylesheet" type="text/css" href="../css/uebung.css">
      <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
      <script type="text/javascript" src="../js/uebungen.js"></script>
