f4d20664115f5bfcc469e17e8d24688116a2398f
Jan Preisig <tiepre@tie.local>
2017-03-08 12:52:26 +0100

    create Uebung

--- subsites/uebungen.php
+++ subsites/uebungen.php
@@ -6,9 +6,9 @@
          ?>
    
     <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/smoothness/jquery-ui.css">
-     <link rel="stylesheet" type="text/css" href="../css/uebung.css">
      <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
      <script type="text/javascript" src="../js/uebungen.js"></script>
+     <link rel="stylesheet" type="text/css" href="../css/uebung.css">
 
     	<title>Lehrverwaltung - Übungen</title>
     	
