c8685209b458ee589548ecbc68653642cf860d28
Jan Preisig <tiepre@tie.local>
2017-03-08 10:20:23 +0100

    timetotal fix

--- subsites/zeitUebersicht.php
+++ subsites/zeitUebersicht.php
@@ -123,6 +123,7 @@ function minToTime($time){
 		$zeit = $row -> zeit;
 		#echo $id."|".$user_id."|".$datum."|".$zeit."<br>";
 		if(mysql_num_rows($select)%2===0){
+			$totalWhileTimerRun = 0;
 		if($counter%2===0){
 			$timetotal+=zeitZuDez($zeit);
 			$zeiten = $zeiten.$zeit;
@@ -133,7 +134,7 @@ function minToTime($time){
 			$zeiten = "";
 			$zeiten = $zeit." bis ";
 			echo "<input type='text' class='form-control' value='";
-		}}
+		}}else{$totalWhileTimerRun = 500;}
 	}
 	echo "</td><td><input class='form-control' type='text'  value='";
 		echo minToTime($timetotal)." h' readonly></td>";
@@ -144,7 +145,7 @@ function minToTime($time){
 }
 
 
-echo "<tr><td></td><td></td><td></td><td></td><td><input type='text' class='form-control' value='Total: ".minToTime($timetotalAll-mysql_num_rows($getdates)*500)." h' readonly></td></tr>";
+echo "<tr><td></td><td></td><td></td><td></td><td><input type='text' class='form-control' value='Total: ".minToTime($timetotalAll-mysql_num_rows($getdates)*500+$totalWhileTimerRun)." h' readonly></td></tr>";
 
 	?>
 		</div>
