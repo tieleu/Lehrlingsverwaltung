05200b21f88ff7cec963523d052f5f25fe259a8a
Jan Preisig <tiepre@tie.local>
2017-03-08 11:31:58 +0100

    create Uebung

--- subsites/uebungen.php
+++ subsites/uebungen.php
@@ -7,6 +7,7 @@
    
   
      <link rel="stylesheet" type="text/css" href="../css/uebung.css">
+     <script type="text/javascript" href="../js/uebungen.js"></script>
 
     	<title>Lehrverwaltung - Übungen</title>
     	
