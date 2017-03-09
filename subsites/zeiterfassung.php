1c78fedb8018858bc81fa0e28884b5b688c2a000
Jan Preisig <tiepre@tie.local>
2017-03-09 13:38:54 +0100

    edit create Uebung

--- subsites/zeiterfassung.php
+++ subsites/zeiterfassung.php
@@ -116,7 +116,7 @@ $totAllColor = "";
 if($timetotalAll-mysql_num_rows($getdates)*500+$totalWhileTimerRun<0){
 	$totAllColor = "#E53427";
 }else{$totAllColor="#3FB13F";}
-echo "<tr><td></td><td></td><td></td><td></td><td><input type='text' class='form-control' value='Total: ".minToTime($timetotalAll-mysql_num_rows($getdates)*500+$totalWhileTimerRun)." h' readonly style='background-color: ".$totAllColor."; font-weight: 5px;'></td></tr>";
+echo "<tr><td></td><td></td><td></td><td></td><td><input type='text' class='form-control' value='Total: ".minToTime($timetotalAll-mysql_num_rows($getdates)*500+$totalWhileTimerRun)." h' readonly style='background-color: ".$totAllColor."; font-weight: bold;'></td></tr>";
 
 	?>
 		</div>
