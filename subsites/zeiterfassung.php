0378cfbf1f049b6df4760abf8cd68fcd17a2e34e
Jan Preisig <tiepre@tie.local>
2017-03-09 13:22:43 +0100

    edit create Uebung

--- subsites/zeiterfassung.php
+++ subsites/zeiterfassung.php
@@ -105,7 +105,9 @@ function minToTime($time){
 		echo minToTime($timetotal)." h' readonly></td>";
 		$timetotalAll += $timetotal;
 		echo "<td><input class='form-control' type='text' value='08:20 h' readonly></td>";
-		echo "<td><input class='form-control' type='text' value='".minToTime($timetotal-500)." h' readonly></td></tr>";
+		echo "<td><input class='form-control' type='text' value='".minToTime($timetotal-500)." h' readonly". if ($timetotal-500<0) {
+			."style='background-color: red;'".
+		}."></td></tr>";
 
 }
 
