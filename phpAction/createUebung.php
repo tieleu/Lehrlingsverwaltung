5f8aeb5c1c6f94038a043a981887ed49cbcd65bc
Jan Preisig <tiepre@tie.local>
2017-03-09 08:13:20 +0100

    edit create Uebung

--- phpAction/createUebung.php
+++ phpAction/createUebung.php
@@ -7,15 +7,11 @@ $uebungsname = $_POST['uebungsname'];
 $thema = $_POST['thema'];
 $voraussetzung = $_POST['voraussetzung'];
 $pfad = $_POST['pfad'];
-echo $uebungsname."<br>";
-echo $thema."<br>";
-echo $voraussetzung."<br>";
-echo $pfad."<br>";
 
 mysql_query("INSERT INTO Uebung (name, Thema, Voraussetzung, Link) VALUES ('$uebungsname', '$thema', '$voraussetzung', '$pfad')")or die("ERROR");
 
 setcookie("createUebung", true, time() + 3, "/");
 
-#header("Location: ../subsites/uebungen.php?user=$user");
+header("Location: ../subsites/uebungen.php?user=$user");
 
 ?>
