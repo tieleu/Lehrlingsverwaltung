b19ceb0bf8069d1ce5bacb890fad9b8c6859eee9
Jan Preisig <tiepre@tie.local>
2017-04-06 08:42:28 +0200

    zeit geflickt und einstellungen bearbeitet

--- subsites/einstellungen.php
+++ subsites/einstellungen.php
@@ -50,6 +50,29 @@ $user = $_GET['user'];
             </form>
     </div>
 
+        <div id="createUser">
+    <form action="../phpAction/createUserAction.php?user=<?php echo $user ?>" method="post">
+    <div id="createUserWrapper">
+        <h2 align="center">Benutzer erstellen</h2>
+        <label for="vorname" class="form-group form-inline labelforPW">Vorname <input class="form-control inputPW" type="text" name="vorname" value="" placeholder="Max" required="true"></label>
+        <label for="nachname" class="form-group form-inline labelforPW">Nachname <input class="form-control inputPW" type="text" name="nachname" value="" placeholder="Muster" required="true"></label>
+        <label for="benutzername" class="form-group form-inline labelforPW">Benutzername <input class="form-control inputPW" type="text" name="benutzername" value="" placeholder="tiemus" required="true"></label>
+        <div class="selectwrapper">
+        <label for="status" class="form-group form-inline labelforPW">Status </label>
+        <select  class="form-control labelforPW" name="status">
+            <option selected>select Status</option>
+            <option value="lehrling">Lehrling</option>
+            <option value="lehrmeister">Lehrmeister</option>
+        </select>
+        </div>
+        <label for="new1" class="form-group form-inline labelforPW">Neues Passwort <input class="form-control inputPW" type="password" name="new1" value="" placeholder="neues Passwort" required="true"></label>
+            <label for="new2" class="form-group form-inline labelforPW">Passwort bestätigen <input class="form-control inputPW" type="password" name="new2" value="" placeholder="Passwort bestätigen" required="true"></label><br><br>
+            </div>
+            <button id="createUserButton" name="creaUsrBtn" class="btn">Benutzer erstellen</button>
+
+            </form>
+    </div>
+
 	<!--<div id="uebungCreate">
     <form action="../phpAction/createUebung.php?user=<?php echo $user?>" method="post" enctype="multipart/form-data">
     <div id= createUebungWrapp>
