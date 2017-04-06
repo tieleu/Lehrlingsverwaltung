2a88abc078629365dca15d90b582ac08e7129c4a
Jan Preisig <tiepre@tie.local>
2017-04-06 08:49:17 +0200

    zeit geflickt und einstellungen bearbeitet

--- subsites/einstellungen.php
+++ subsites/einstellungen.php
@@ -54,6 +54,40 @@ $user = $_GET['user'];
     <form action="../phpAction/deleteUserAction.php?user=<?php echo $user ?>" method="post">
     <div id="deleteUserWrapper">
         <h2 align="center">Benutzer löschen</h2>
+
+        <div class="panel-heading">
+            <?php
+            $ausgabe = "SELECT vorname, username from User where status = 'lehrling'";
+            $ergebniss = mysql_query($ausgabe);
+            ?>
+            <form action="" method="post">
+                <select id="select" name="select" onchange="this.form.submit()" style="width: 170px; height: 35px;">
+                    <option disabled hidden selected="selected">Auswahl</option>
+                    <?php
+
+                    $selectOption = $_POST['select'];
+                    
+                    while ($row = mysql_fetch_object($ergebniss)) {
+                        $vorname = $row -> vorname;
+                        
+                        if($vorname == $selectOption){
+                            ?>
+                            <div class="panel-heading">
+                                <option selected="selected"><?php echo  $vorname  ?></option>
+                            </div>
+                            <?php
+                        } else {
+                            ?>  
+                            <div class="panel-heading">
+                                <option><?php echo  $vorname  ?></option>
+                            </div>
+                            <?php } ?>
+
+                            <?php } ?>
+
+                        </select>
+                    </form>
+                </div>
         
             </div>
             <button id="createUserButton" name="creaUsrBtn" class="btn">Benutzer erstellen</button>
