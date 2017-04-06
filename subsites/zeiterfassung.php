72b94556ee0e4c0c779171e17b94af38a2b3ec20
Jan Preisig <tiepre@tie.local>
2017-04-06 08:39:50 +0200

    rollback zeitänderungen

--- subsites/zeiterfassung.php
+++ subsites/zeiterfassung.php
@@ -110,7 +110,9 @@ if(mysql_num_rows($check)>0 && mysql_num_rows($check)!=null){
 
 					}
 					$totAllColor = "";
-					#if(mysql_num_rows($select)%2===0){$totalWhileTimerRun=500;}
+					if(mysql_num_rows($select)%2===0){$totalWhileTimerRun=500;}else{
+						$totalWhileTimerRun=0;
+					}
 					if($timetotalAll-mysql_num_rows($getdates)*500+$totalWhileTimerRun<0){
 						$totAllColor = "#E53427";
 					}else{$totAllColor="#3FB13F";}
