5e8f99423dd3fb5519088c5f897af92a319b0f4e
Jan Preisig <tiepre@tie.local>
2017-04-06 10:15:37 +0200

    einstellungen del user

--- phpAction/deleteUserAction.php
+++ phpAction/deleteUserAction.php
@@ -3,7 +3,7 @@ session_start();
 include('../subsites/header.php');
 $user = $_GET['user'];
 if (isset($_POST['no'])) {
-	echo "no";
+	echo $_POST['select'];
 }else if(isset($_POST['yes'])){
 	echo "yes";
 }
