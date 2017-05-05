d559935db40bae08ed884b01c614b17b24b7c9a8
Jan Preisig <tiepre@tie.local>
2017-05-05 09:15:38 +0200

    zeittotal rot grün geflickt

--- subsites/zeiterfassung.php
+++ subsites/zeiterfassung.php
@@ -120,17 +120,17 @@ if(mysql_num_rows($check)>0 && mysql_num_rows($check)!=null){
 						if($date =="2017-04-24"){
 
 							echo "<td><input class='form-control' type='text' value='04:10 h' readonly></td>";
-							echo "<td><input class='form-control' type='text' value='".minToTime($totalTime-250)." h' readonly style='border: solid 2px ".totalColor($solltime-250).";'></td></tr>";
+							echo "<td><input class='form-control' type='text' value='".minToTime($totalTime-250)." h' readonly style='border: solid 2px ".";'></td></tr>";
 							$totalTimeAll += $totalTime-250;
 							$feiertagMal500 += 500;
 						}else if($date =="2017-04-13"){
 							echo "<td><input class='form-control' type='text' value='06:15 h' readonly></td>";
-							echo "<td><input class='form-control' type='text' value='".minToTime($totalTime-375)." h' readonly style='border: solid 2px ".totalColor($solltime-125).";'></td></tr>";
+							echo "<td><input class='form-control' type='text' value='".minToTime($totalTime-375)." h' readonly style='border: solid 2px ".";'></td></tr>";
 							$totalTimeAll += $totalTime-375;
 							$feiertagMal500 += 500;
 						}else{
 							echo "<td><input class='form-control' type='text' value='08:20 h' readonly></td>";
-							echo "<td><input class='form-control' type='text' value='".minToTime($totalTime-$solltime)." h' readonly style='border: solid 2px ".totalColor($solltime).";'></td></tr>";
+							echo "<td><input class='form-control' type='text' value='".minToTime($totalTime-$solltime)." h' readonly style='border: solid 2px ".";'></td></tr>";
 							$totalTimeAll += $totalTime;
 						}					
 					}
