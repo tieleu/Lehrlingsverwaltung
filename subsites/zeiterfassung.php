5c698c08ab0d2e49301c7f40de11a21a3610b170
Jan Preisig <tiepre@tie.local>
2017-05-05 10:01:47 +0200

    zeittotal rot grün geflickt

--- subsites/zeiterfassung.php
+++ subsites/zeiterfassung.php
@@ -121,7 +121,8 @@ if(mysql_num_rows($check)>0 && mysql_num_rows($check)!=null){
 
 						if($date =="2017-04-24"){
 							$color = totalColor($solltime-250);
-							echo ",test".$totalTime." ,".$solltime-250;
+							echo ",test".$totalTime." ,";
+							echo "<br>solltime".$solltime-250;
 							echo "<td><input class='form-control' type='text' value='04:10 h' readonly></td>";
 							echo "<td><input class='form-control' type='text' value='".minToTime($totalTime-250)." h' readonly style='border: solid 2px ".$color.";'></td></tr>";
 							$totalTimeAll += $totalTime-250;
