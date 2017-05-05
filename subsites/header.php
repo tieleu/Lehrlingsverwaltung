623753d9b3b0381722174c70d67c94097593aff2
Jan Preisig <tiepre@tie.local>
2017-05-05 11:48:24 +0200

    zeitUebersicht verschmalert

--- subsites/header.php
+++ subsites/header.php
@@ -33,7 +33,7 @@ if($_SESSION['eingeloggt']== true && $nameOfUser == $sessionUser){
 		<link rel="stylesheet" type="text/css" href="../css/index.css">
 
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-		<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 
 		<script type="text/javascript" src="../js/keepalive.js"></script>
