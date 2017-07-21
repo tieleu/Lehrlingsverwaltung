80c969592e21e3792b9975d4f2b5a7223ae1e382
Jan Preisig <tiepre@tie.local>
2017-07-21 08:56:41 +0200

    patterns erweitert

--- js/patterns.js
+++ js/patterns.js
@@ -1,7 +1,11 @@
 $(document).ready(function(){
 	$("#strategyUml").click(function(){
 		$("#strategyUml").toggleClass("zoom");
-	})
+	});
+
+	$('#builderUml').click(function(){
+		$('#builderUml').toggleClass('zoom');
+	});
 });
 /*while (true) {
 	console.log("hack");
--- subsites/patterns.php
+++ subsites/patterns.php
@@ -55,7 +55,7 @@
 			<h3 id="strategyPattern">Strategy Pattern</h3>
 			<div id="wrappStrategy">
 				<div class="wrappCode" id="codeStrategy1">
-					<img id="strategyUml" src="../image/patterns/strategy/strategy_uml.jpg" alt="strategy pattern uml diagram">
+					<img id="strategyUml" class="umlCursor" src="../image/patterns/strategy/strategy_uml.jpg" alt="strategy pattern uml diagram">
 					<h4>Step 1</h4>
 					<p>Create an Interface -> FlyAbility.java</p>
 					<img src="../image/patterns/strategy/strategy_code_1.jpg">
@@ -80,7 +80,23 @@
 			<h3 id="builderPattern">Builder Pattern</h3>
 			<div id="wrappBuilder">
 				<div class="wrappCode" id="codeBuilder1">
-					<img id="builderUml" src="../img/patterns/builder/builder_uml.jpg" alt="builder_pattern_uml">
+					<img id="builderUml" class="umlCursor" src="../image/patterns/builder/builder_uml.jpg" alt="builder_pattern_uml">
+					<h4>Step 1</h4>
+					<p>Create a Class of the Object to build -> Room.java</p>
+					<img src="../image/patterns/builder/builder_1_1.jpg">
+					<img src="../image/patterns/builder/builder_1_2.jpg">
+					<img src="../image/patterns/builder/builder_1_3.jpg">
+					<img src="../image/patterns/builder/builder_1_4.jpg">
+				</div>
+				<div class="wrappCode" id="codeBuilder2">
+					<h4>Step 2</h4>
+					<p>Create the Builder Class which Bilds an Object of our previous created Class</p>
+					<img src="../image/patterns/builder/builder_2_1.jpg">
+					<img src="../image/patterns/builder/builder_2_2.jpg">
+					<h4>Step 3</h4>
+					<p>Eine Main Klasse erstellen welche ein Builder Objekt macht und das Objekt mit dem Builder Vorbereitet. Danach die Build methode ausführt und ein Objekt bekommt.</p>
+					<img src="../image/patterns/builder/builder_3.jpg">
+					<p>Ein Codebeispiel zur Implementierung gibt es <a href="../data/patterns/builderRoomBeispiel.zip">hier</a></p>
 				</div>
 				
 			</div>
