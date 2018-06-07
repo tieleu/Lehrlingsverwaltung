f3e5b2b0b65b7f30a4245fd6945438d19c2caed5
tiepre <jan.preisig@tie.ch>
2018-06-07 17:30:26 +0200

    chat

--- index.php
+++ index.php
@@ -106,7 +106,7 @@ if($_SESSION['eingeloggt']== true && $nameOfUser == $sessionUser){
 				<script src="animation/js/EasePack.min.js"></script>
 				<script src="animation/js/rAF.js"></script>
 				<script src="animation/js/demo-1.js"></script>
-				<i class="fa fa-space-shuttle fa-spin" id="space-shuttle" aria-hidden="true"></i>
+				<a href="chat/ajax-chat.php"><i class="fa fa-space-shuttle fa-spin" id="space-shuttle" aria-hidden="true"></i></a>
 				<i class="fa fa-space-shuttle fa-spin" id="space-shuttle2" aria-hidden="true"></i>
 				<i class="fa fa-space-shuttle fa-spin" id="space-shuttle3" aria-hidden="true"></i>
 				<i class="fa fa-space-shuttle fa-spin" id="space-shuttle4" aria-hidden="true"></i>
